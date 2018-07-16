9fc43b10b53f482fef53bbe653b1c56706c9c496
Sebass van Boxel <svboxel@gmail.com>
2018-07-16 11:53:34 +0200

    Initial setup delete-merged-branch-bot

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * This is the entry point for your Probot App.
+ * @param {import('probot').Application} app - Probot's Application class.
+ */
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
