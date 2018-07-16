efd03105680e2e84668f9f02d38605fce73f6a9d
Sebass van Boxel <sebass@wetransfer.com>
2018-07-16 15:32:18 +0200

    Delete branch functionality

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
+  app.on(`pull_request.closed`, async context => {
+    const owner = context.payload.repository.owner.login
+    const repo = context.payload.repository.name
+    const ref = `heads/${context.payload.pull_request.head.ref}`
+    console.log({ owner, repo, ref })
 
+    const result = await context.github.gitdata.deleteReference({ owner, repo, ref })
+    console.log(result)
+    // return context.github.issues.createComment(params)
+  })
   // For more information on building apps:
   // https://probot.github.io/docs/
 
