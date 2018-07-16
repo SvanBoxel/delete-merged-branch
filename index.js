0cd009dfec7ef7b0f52e409d639b61d150a068fa
Sebass van Boxel <sebass@wetransfer.com>
2018-07-16 17:23:05 +0200

    Improved doc styling

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const deleteMergedBranch = require('./lib/delete-merged-branch')
 
-/**
- * This is the entry point for your Probot App.
- * @param {import('probot').Application} app - Probot's Application class.
- */
 module.exports = app => {
   app.log('Yay, the app was loaded!')
   app.on(`pull_request.closed`, deleteMergedBranch)
