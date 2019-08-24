41559ffe0e1fcf3e4dced652d90a835ce5736e04
Nishchal Gautam <cyberhck@users.noreply.github.com>
2019-08-24 14:45:29 +0700

    style(quotes): fix lint issue

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const deleteMergedBranch = require('./lib/delete-merged-branch')
 
 module.exports = app => {
   app.log('Loaded delete-merged-branch GitHub Application')
-  app.on(`pull_request.closed`, deleteMergedBranch)
+  app.on('pull_request.closed', deleteMergedBranch)
 }
