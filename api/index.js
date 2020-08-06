9be5601b24ad0afcb6a5e8f22bd210a63bdf7b97
Sebass van Boxel <svboxel@gmail.com>
2020-08-06 13:54:35 +0200

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const deleteMergedBranch = require('../lib/delete-merged-branch')
+
+module.exports = app => {
+  app.log('Loaded delete-merged-branch GitHub Application')
+  app.on('pull_request.closed', deleteMergedBranch)
+}
