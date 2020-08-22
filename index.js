8b30ceec2c3243e942ae5bf96a26f1b173481aa8
Sebass van Boxel <svboxel@gmail.com>
2020-08-22 22:11:43 +0200

    Fix secret errors in gcp

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const deleteMergedBranch = require('./lib/delete-merged-branch')
 
 module.exports = app => {
-  const { APP_ID, WEBHOOK_SECRET, PRIVATE_KEY } = process.env
-  console.log(555, APP_ID, WEBHOOK_SECRET, PRIVATE_KEY);
-
   app.log('Loaded delete-merged-branch GitHub Application')
   app.on('pull_request.closed', deleteMergedBranch)
 }
